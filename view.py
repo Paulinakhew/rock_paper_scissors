c43d767c3fd6f3bf8b206e8d0d202d472392d1b8
Esther Khew <paulinakhew12345@hotmail.com>
2018-11-12 21:53:50 -0500

    created view menu function

--- view.py
+++ view.py
@@ -5,3 +5,6 @@ import os
 def clear_screen():
     os.system('clear')
 
+def welcome_menu():
+    print("""Welcome to rock, paper, scissors! 
+To play, enter the first letter of your choice when prompted. The computer's move is randomly generated.""")
